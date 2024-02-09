a6c8800fa7e3447911fef8e64df342398fc6f689
Amady <123684542+misterbb38@users.noreply.github.com>
2024-02-09 18:20:57 +0100

    correcion 6

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const cors = require('cors'); // Importer cors
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 
@@ -11,7 +12,9 @@ dotenv.config();
 // Connexion à la base de données
 connectDB();
 
-const app = express();
+const app = express(); // Initialisation de l'app Express
+
+app.use(cors()); // Activer CORS pour toutes les requêtes
 
 // Body parser pour lire les données du corps de la requête
 app.use(express.json());
@@ -22,12 +25,10 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Routes
-
 const invoice = require('./routes/invoiceRoutes');
 const client = require('./routes/clientRoutes');
 
 // Monter les routeurs
-
 app.use('/api/invoice', invoice);
 app.use('/api/client', client);
 
@@ -36,4 +37,6 @@ app.use(errorHandler);
 
 // Démarrage du serveur
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Le serveur écoute sur le port ${PORT} et fonctionne en mode ${process.env.NODE_ENV}`.yellow.bold.underline));
+app.listen(PORT, () => {
+    console.log(`Le serveur écoute sur le port ${PORT} et fonctionne en mode ${process.env.NODE_ENV}`.yellow.bold.underline);
+});

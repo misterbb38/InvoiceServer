3b8bc275c045e0940b14df46a2fd3eb4ad58fc92
Amady <123684542+misterbb38@users.noreply.github.com>
2024-02-09 18:48:34 +0100

    correction cors

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ connectDB();
 
 const app = express(); // Initialisation de l'app Express
 
-app.use(cors()); // Activer CORS pour toutes les requêtes
+app.use(cors({
+    origin: 'https://invoice-frontend-lw94.vercel.app/', // Autoriser seulement cette origine à accéder à l'API
+}));
+
 
 // Body parser pour lire les données du corps de la requête
 app.use(express.json());
